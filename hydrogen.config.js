f8f70a58cf92e4a898b133b122666985065ca8bb
Carlstong <carlstongrefalde@gmail.com>
2022-09-22 22:01:24 +0800

    added free items

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -4,8 +4,8 @@ export default defineConfig({
   shopify: {
     defaultCountryCode: 'US',
     defaultLanguageCode: 'EN',
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'crankycards.myshopify.com',
+    storefrontToken: 'f0e43658848942d7a8904584092763ab',
     storefrontApiVersion: '2022-07',
   },
   session: CookieSessionStorage('__session', {
